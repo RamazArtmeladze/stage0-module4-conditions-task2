07b39b7b953655d058a0a931eddf52c3d7d9715f
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-20 15:12:34 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        if (from1UpTo1000 < 500) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
+    }
 
+    public static void main(String[] args) {
+        CoinFlip coinFlip = new CoinFlip();
+        coinFlip.throwCoin(250); // Output will be "Eagle"
+        coinFlip.throwCoin(750); // Output will be "Tail"
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first >= second) {
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
+    }
+
+    public static void main(String[] args) {
+        GreatestNumberPrinter printer = new GreatestNumberPrinter();
+        printer.printGreatest(5, 10);  // Output will be "10"
+        printer.printGreatest(7, 7);   // Output will be "7"
+        printer.printGreatest(20, 15); // Output will be "20"
     }
 }
